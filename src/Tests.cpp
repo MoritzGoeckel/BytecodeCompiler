69e0a6a6b0234eaed42227ca9012aa0fe73a78e2
Moritz Goeckel <moritz.goeckel@gmail.com>
2018-12-19 23:27:00 +0100

    Removed some byte files

--- src/Tests.cpp
+++ src/Tests.cpp
@@ -15,10 +15,10 @@
 TEST(Writer, WriteFileTest) {
   std::vector<unsigned char> outData = {15, 30};
   ByteCode outCode(outData);
-  outCode.write("testfile.bytes");
+  outCode.write("testfile_vec.bc");
 
   ByteCode inCode;
-  inCode.read("testfile.bytes");
+  inCode.read("testfile_vec.bc");
 
   EXPECT_EQ (15, inCode.getAt(0));
   EXPECT_EQ (30, inCode.getAt(1));
@@ -26,12 +26,12 @@ TEST(Writer, WriteFileTest) {
 }
 
 TEST(Compiler, ReadAndCompile) {
-  compileFile("testfile.ma", "testfile.out");
+  compileFile("testfile.ma", "testfile.bc");
 }
 
 TEST(MVM, ReadAndExecute) {
   ByteCode code;
-  code.read("testfile.out");
+  code.read("testfile.bc");
   VirtualMachine::execute(code);
   std::cout << "Done executing" << std::endl;
 }
